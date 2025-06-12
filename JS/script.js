e7171f7536670f5ada266c4c995c33b9b7197626
aniketsardar47 <aniketsardar89@gmail.com>
2025-06-12 09:36:51 +0530

    first commit

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+var a = 0;
+
+while(a != 10){
+    console.log("Hello");
+    a++;
+}
+
+for(let i=0;i<=10;i++){
+    console.log("Hello");
+}
+
+let x = (n)=>{
+    return n*n;
+}
+
+let c = x(2);
+
+console.log(c);
